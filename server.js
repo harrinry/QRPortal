be1ef54919a90d8e171bc2b306c546db17eeb9a6
Guillaume Rager <grager@prg-consulting.com>
2018-02-15 15:23:47 +0100

    added node-static module

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var path = require('path');
+var static = require('node-static');
 
 var port = process.env.PORT || 8080;
 
 var app = express();
 app.use(express.static('QRPortal'));
 app.use(express.static('static'));
+
+
+var file = new static.Server('./extensions');
+ 
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(port);
+
 app.get('/default.html', function(req, res) {
     res.sendFile(path.join(__dirname + '/static/default.html'));
 });
