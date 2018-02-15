483463d161a458058744e0ce66ca5e5d568e4e48
Guillaume Rager <grager@prg-consulting.com>
2018-02-15 18:46:56 +0100

    ready to be released

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('QRPortal'));
 app.use(express.static('static'));
 
 
-var file = new static.Server('./extensions');
+/*var file = new static.Server('./extensions');
  
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
@@ -18,16 +18,57 @@ require('http').createServer(function (request, response) {
         //
         file.serve(request, response);
     }).resume();
-}).listen(port);
-/*
+}).listen(port);*/
+
 app.get('/default.html', function(req, res) {
     res.sendFile(path.join(__dirname + '/static/default.html'));
 });
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/root.json'));
 });
+
+app.get('/js/*', function(req, res) {
+	
+	console.log("index>"+req.url);
+	
+	var urlfull = req.url;
+	
+   res.sendFile(path.join(__dirname + urlfull/*.replace('.html','')*/));
+//    res.sendFile(path.join(__dirname + req.url));
+});
+
+app.get('/css/*', function(req, res) {
+	
+	console.log("index>"+req.url);
+	
+	var urlfull = req.url;
+	
+   res.sendFile(path.join(__dirname + urlfull/*.replace('.html','')*/));
+//    res.sendFile(path.join(__dirname + req.url));
+});
+
+app.get('/extensions/*/*', function(req, res) {
+	
+	console.log("index>"+req.url);
+	
+	var urlfull = req.url;
+	
+   res.sendFile(path.join(__dirname + urlfull/*.replace('.html','')*/));
+//    res.sendFile(path.join(__dirname + req.url));
+});
+
+app.get('/*/*.json', function(req, res) {
+		
+    res.sendFile(path.join(__dirname + req.url));
+});
+
+app.get('/*/*/*.json', function(req, res) {
+		
+    res.sendFile(path.join(__dirname + req.url));
+});
+
 app.listen(port, function() {
 
 	console.log("Listening..."+port);	
 	
-});*/
+});
