295722808298de5947829269f39c0e61a6088461
Guillaume Rager <grager@prg-consulting.com>
2018-02-15 15:07:05 +0100

    dynamic port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
 
+var port = process.env.PORT || 8080;
+
 var app = express();
 app.use(express.static('QRPortal'));
 app.use(express.static('static'));
@@ -10,5 +12,8 @@ app.get('/default.html', function(req, res) {
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/root.json'));
 });
-app.listen(8080);
-console.log("Listening 8080...");
+app.listen(port, function() {
+
+	console.log("Listening..."+port);	
+	
+});
