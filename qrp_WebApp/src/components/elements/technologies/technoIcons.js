dea0776a65d503aa3062944020f39a2210058776
Guillaume Rager <grager@prg-consulting.com>
2018-04-17 11:48:22 +0200

    not working

--- qrp_WebApp/src/components/elements/technologies/technoIcons.js
+++ qrp_WebApp/src/components/elements/technologies/technoIcons.js
@@ -1,3 +1,10 @@
 export const ICONURLS = {
-  
+
+  jee:'https://raw.githubusercontent.com/CAST-Extend/resources/master/com.castsoftware.jee.png',
+  html5:'https://raw.githubusercontent.com/CAST-Extend/resources/master/com.castsoftware.html5.png'
+
+/*
+
+*/
+
 };
