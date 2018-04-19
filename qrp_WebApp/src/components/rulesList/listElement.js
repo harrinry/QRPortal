0fd6307254c802ea6392271566650f9fc060c127
Guillaume Rager <grager@prg-consulting.com>
2018-04-19 15:47:46 +0200

    added critical image in the list of rules

--- qrp_WebApp/src/components/rulesList/listElement.js
+++ qrp_WebApp/src/components/rulesList/listElement.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Radio, lOADDETAILS, UNSELECTME, SELECTME} from '../index';
 
-const localClassName = 'table-row', cellClass = 'table-cell'/*, prefix = 'letr_'*/;
+const localClassName = 'table-row', cellClass = 'table-cell', criticalClass = 'critical-cell'/*, prefix = 'letr_'*/;
 
 export default class RuleListRowElement extends React.Component{
   constructor(props){
@@ -14,16 +14,16 @@ export default class RuleListRowElement extends React.Component{
 
     Radio.listen(UNSELECTME, () => {
       if( this.state.selected ){
-        this.setState( _state => { 
-          return { href: _state.href, selected: false}; 
+        this.setState( _state => {
+          return { href: _state.href, selected: false};
         });
       }
     });
 
     Radio.listen(SELECTME, ( url ) => {
       if( this.state.href === url){
-        this.setState( _state => { 
-          return { href: _state.href, selected: true}; 
+        this.setState( _state => {
+          return { href: _state.href, selected: true};
         });
       }
     });
@@ -41,7 +41,7 @@ export default class RuleListRowElement extends React.Component{
         return this.dispatchLoadDetails();}.bind(this)}>
         <td className={cellClass}>{this.props.el.id}</td>
         <td className={cellClass}>{this.props.el.name}</td>
-        <td className={cellClass}>{ this.props.el.critical ? this.props.el.critical.toString() : undefined}</td>
+        <td className={cellClass}><span className={this.props.el.critical ? criticalClass : undefined}>&nbsp;</span></td>
       </tr>
     );
   }
