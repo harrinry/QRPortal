4c7f7229d5611495c42d00be4e20c05244f5d524
Guillaume Rager <grager@prg-consulting.com>
2018-04-19 19:56:10 +0200

    show/hide blocks depending on the availability of the content

--- qrp_WebApp/src/components/rulesList/details.js
+++ qrp_WebApp/src/components/rulesList/details.js
@@ -6,50 +6,38 @@ const localClassName = ['RuleInfo-container', 'R50-margins'],
 export default class RuleDetails extends React.Component{
   render(){
     if (this.props.data){
+
+      const criticalblock = this.props.data.critical ? (<div className='critical-container'>&nbsp</div>) : ('');
+      const weightblock = this.props.data.weight ? (<div className='weight-container'>{this.props.data.weight}</div>) : ('');
+      const remediationblock = this.props.data.remediation ? (<div className='remediation-container detailssection'><p className='rulesection'>Remediation</p><p>{this.props.data.remediation}</p></div>) : ('');
+      const rationaleblock = this.props.data.rationale ? (<div className='rationale-container detailssection'><p className='rulesection'>Rationale</p><p>{this.props.data.rationale}</p></div>) : ('');
+      const sampleblock = this.props.data.sample ? (<div className='sample-container detailssection'><p className="rulesection">Sample</p><pre><code>{this.props.data.sample}</code></pre></div>) : ('');
+      const remediationsampleblock = this.props.data.remediationSample ? (<div className='remediationsample-container detailssection'><p className='rulesection'>Remediation Sample</p><pre><code>{this.props.data.remediationSample}</code></pre></div>) : ('');
+      const referenceblock = this.props.data.reference ? (<div className='reference-container detailssection'><p className='rulesection'>Reference</p><p className='textrule'>{this.props.data.reference}</p></div>) : ('');
+      const outputblock = this.props.data.output ? (<div className='output-container detailssection'><p className='rulesection'>Output</p><p className='textrule'>{this.props.data.output}</p></div>) : ('');
+
       return (
         <div className={localClassName.join( sp )}>
+        {criticalblock}
+        {weightblock}
           <h2 className='ruleTitle'>{this.props.data.name}</h2>
           <div className='tags-container'>
             <ul>{this.props.data.qualityStandards.id}</ul>
           </div>
-          <div className='weight-container'>
-            {this.props.data.weight}
-          </div>
-          <div className='critical-container'>
-            {this.props.data.critical ? 'nbsp;':undefined}
-          </div>
-          <div className='description-container'>
+          <div className='description-container detailssection'>
             <p className='rulesection'>Description</p>
             <p>{this.props.data.description}</p>
           </div>
-          <div className='rationale-container'>
-            <p className='rulesection'>Rationale</p>
-            <p>{this.props.data.rationale}</p>
-          </div>
-          <div className='remediation-container'>
-            <p className='rulesection'>Remediation</p>
-            <p>{this.props.data.remediation}</p>
-          </div>
-          <div className='sample-container'>
-            <p className="rulesection">Sample</p>
-            <pre><code>{this.props.data.sample}</code></pre>
-          </div>
-          <div className='remediationsample-container'>
-            <p className='rulesection'>Remediation Sample</p>
-            <pre><code>{this.props.data.remediationSample}</code></pre>
-          </div>
-          <div className='reference-container'>
-            <p className='rulesection'>Reference</p>
-            <p className='textrule'>{this.props.data.reference}</p>
-          </div>
-          <div className='output-container'>
-            <p className='rulesection'>Output</p>
-            <p className='textrule'>{this.props.data.output}</p>
-          </div>
+          {rationaleblock}
+          {remediationblock}
+          {sampleblock}
+          {remediationsampleblock}
+          {referenceblock}
+          {outputblock}
         </div>
       );
     }
-    return (<div></div>);
+    return (<div className="noruleselected">No Rule Selected</div>);
   }
 
   parseCodeSample(sample){
