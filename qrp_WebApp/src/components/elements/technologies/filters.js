9cb9ed49f5fbfaec23cb5962d92834bf234d6f5e
Guillaume Rager <grager@prg-consulting.com>
2018-04-27 15:52:03 +0200

    mixing PLI and PLC data

--- qrp_WebApp/src/components/elements/technologies/filters.js
+++ qrp_WebApp/src/components/elements/technologies/filters.js
@@ -1,9 +1,10 @@
 const filters = {
   cpp: [ -2, -3, 1050571 ],
   dotNet: [141901],
-  rpg: [1008000, 1009000, 1011000, 1012000]
+  rpg: [1008000, 1009000, 1011000, 1012000],
+  pli: [1004000, 1005000]
 };
 
-filters.all = [].concat(...filters.cpp, ...filters.dotNet, ...filters.rpg);
+filters.all = [].concat(...filters.cpp, ...filters.dotNet, ...filters.rpg, ...filters.pli);
 
 export default filters;
--- qrp_WebApp/src/modules/filter.js
+++ qrp_WebApp/src/modules/filter.js
@@ -4,9 +4,10 @@ const filters = {
   cpp: [ -2, -3, 1050571 ],
   dotNet: [141901],
   rpg: [1008000, 1009000, 1011000, 1012000],
+  pli: [1004000, 1005000],
 };
 
-filters.all = [].concat(...filters.cpp, ...filters.dotNet, ...filters.rpg);
+filters.all = [].concat(...filters.cpp, ...filters.dotNet, ...filters.rpg,...filters.pli);
 
 function isCPP( id ){
   return filters.cpp.indexOf( id ) === -1 ? false : true;
@@ -24,17 +25,25 @@ function isRPG( id ){
   return filters.rpg.indexOf( id ) === -1 ? false : true;
 }
 
+function getPLIEntries( arr ){
+  return arr.filter( en => isPLI(en.id));
+}
+
+function isPLI( id ){
+  return filters.pli.indexOf( id ) === -1 ? false : true;
+}
+
 function isPure( id ){
   return filters.all.indexOf( id );
 }
 
 export default function filter( arr ){
   const ID = 'id';
-  let cpp, dotNet, rpm;
+  let cpp, dotNet, rpm, pli;
 
   const filtered = arr.map( e => {
     const idx = isPure( e.id );
-    
+
     if (idx !== -1) {
       if (idx < 3 && !cpp) {
         cpp = true;
@@ -48,7 +57,7 @@ export default function filter( arr ){
       } else if( idx === 3 && !dotNet){
         dotNet = true;
         return;
-      } else if ( idx > 3 && !rpm ) {
+      } else if ( idx > 3 && idx < 8 && !rpm ) {
         rpm = true;
         const us = getRPGEntries( arr );
         const urls = us.map( a => a.href );
@@ -56,6 +65,14 @@ export default function filter( arr ){
         e.id = filters.rpg[0];
         e.href = newHref;
         e.name = 'RPG';
+      } else if ( idx > 7 && !pli ) {
+        pli = true;
+        const us = getPLIEntries( arr );
+        const urls = us.map( a => a.href );
+        const newHref = MultiQuery( ID, ...urls );
+        e.id = filters.pli[0];
+        e.href = newHref;
+        e.name = 'PLI';
       } else {
         return;
       }
