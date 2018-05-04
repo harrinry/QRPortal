2c4843f7f1a32e0a7cef0c30cc94fa42a4b7b678
Guillaume Rager <grager@prg-consulting.com>
2018-05-04 15:50:43 +0200

    updated filters

---
+++ qrp_WebApp/src/components/elements/technologies/filters.js
@@ -0,0 +1,12 @@
+const filters = {
+  cpp: [ -2, -3, 1050571 ],
+  dotNet: [141901],
+  rpg: [1008000, 1009000, 1011000, 1012000],
+  pli: [1004000, 1005000],
+  mssql: [-13,140998],
+  sap: [-15,-20],
+};
+
+filters.all = [].concat(...filters.cpp, ...filters.dotNet, ...filters.rpg, ...filters.pli, ...filters.mssql, ...filters.sap);
+
+export default filters;
