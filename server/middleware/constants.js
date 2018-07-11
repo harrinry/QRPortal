88aec711d4ec682afc86fa453a6334e178fe056f
MSU <m.stura@castsoftware.com>
2018-07-11 11:06:11 +0200

    added swagger-ui static route

--- server/middleware/constants.js
+++ server/middleware/constants.js
@@ -2,13 +2,19 @@ const path = require('path');
 
 module.exports = {
   poweredByHeader: 'x-powered-by',
-  staticRoutes: {
-    static: {
+  staticRoutes: [
+    {
       name: 'static',
       dir: 'static',
       url: '/default.html',
       PATH: path.resolve(__dirname, '..', '..', 'static')
+    },
+    {
+      name: 'swagger-ui',
+      dir: 'swagger-ui',
+      url: '/swagger.html',
+      PATH: path.resolve(__dirname, '..', '..', 'swagger-ui')
     }
-  }
+  ]
 };
 
