8343052ef4c4d1fecd9eacb983d64b1cac88636f
MSU <m.stura@castsoftware.com>
2018-07-27 10:30:57 +0200

    wip

---
+++ public/src/view-navigation/index.js
@@ -0,0 +1,16 @@
+import ACTIONS from './vn-actions';
+import ACTIONTYPES from './vn-actions-type';
+import CONSTANTS from './vn-constants';
+import Component  from './vn-model';
+import reducers from './vn-reducers';
+import Resources from './vn-resources';
+
+
+export const viewNagivation = {
+  CONSTANTS: CONSTANTS,
+  ACTIONS: ACTIONS,
+  ACTIONTYPE: ACTIONTYPES,
+  COMPONENT: Component,
+  REDUCERS: reducers,
+  RESOURCSES: Resources
+};
