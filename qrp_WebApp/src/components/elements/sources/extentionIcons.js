e7400e822398841f127b961eca899fc943514c2b
MSU <m.stura@castsoftware.com>
2018-07-30 15:10:42 +0200

    updated icon mappings

--- qrp_WebApp/src/components/elements/sources/extentionIcons.js
+++ qrp_WebApp/src/components/elements/sources/extentionIcons.js
@@ -28,5 +28,6 @@ export const EXTENTIONICONS = {
   'cpp': 'https://raw.githubusercontent.com/CAST-Extend/resources/master/com.castsoftware.cfamily.png',
   'wbslinker': 'https://raw.githubusercontent.com/CAST-Extend/resources/master/com.castsoftware.wbslinker.png',
   'nosqljava': 'https://raw.githubusercontent.com/CAST-Extend/resources/master/com.castsoftware.nosqljava.png',
-  'jeerules': 'https://raw.githubusercontent.com/CAST-Extend/resources/master/com.castsoftware.jeeqr.png'
+  'jeerules': 'https://raw.githubusercontent.com/CAST-Extend/resources/master/com.castsoftware.jeeqr.png',
+  'typescript': 'https://raw.githubusercontent.com/CAST-Extend/resources/master/com.castsoftware.typescriptangular.png'
 };
