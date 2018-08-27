776b3f33346278f6af5442d6017dc80843bed184
MSU <m.stura@castsoftware.com>
2018-08-27 11:38:35 +0200

    prep_updates

--- server/settings/routes.js
+++ server/settings/routes.js
@@ -28,20 +28,5 @@ module.exports = [
     name: 'about',
     route: '/about',
     router: 'router'
-  },
-  {
-    name: 'temp',
-    route: '/temp',
-    router: 'router'
-  },
-  {
-    name: 'admin',
-    route: '/admin',
-    router: 'router'
-  },
-  {
-    name: 'marketing',
-    route: '/mkt',
-    router: 'router'
   }
 ];
