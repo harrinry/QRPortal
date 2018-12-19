4d348f1f959dde1661b945d9fe30a3de256aab8c
Matteo Stura <MSU@corp.castsoftware.com>
2018-12-19 18:33:22 +0100

    update

--- server/lib/navigation-map.js
+++ server/lib/navigation-map.js
@@ -3,19 +3,19 @@ const navData = [
     name: 'Health Factors',
     href: 'AIP/business-criteria',
     icon: 'img/castsoftware.svg',
-    info: 'Browse best practices provided by CAST by health facotrs'
+    info: 'Browse rules provided by CAST by health facotrs'
   },
   {
     name: 'Standards',
     href: 'AIP/quality-standards',
     icon: '/img/standards.svg',
-    info: 'Browse best practices provided by CAST according to the standard they meet: CAST, CISQ, OWASP, CWE'
+    info: 'Browse rules provided by CAST according to the standard they meet: CAST, CISQ, OWASP, CWE'
   },
   {
     name: 'Technologies',
     href: 'AIP/technologies',
     icon: '/img/technologies.svg',
-    info: 'Browse best practices provided by CAST according to the technology they target'
+    info: 'Browse rules provided by CAST according to the technology they target'
   },
   // {
   //   name: 'Packages',
