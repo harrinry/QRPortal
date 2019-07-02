7c9722f8aaa3e4e3a1c79874d9ed4a7f209f5f16
Guillaume Rager <g.rager@castsoftware.com>
2019-07-02 20:58:42 +0200

    details view updated for best practice

--- public/src/details-section/ds-model.js
+++ public/src/details-section/ds-model.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
-import { createClassName, COMMON_CLASSES } from '../common/';
+import { createClassName, COMMON_CLASSES, isEcho } from '../common/';
 import { CLASSES, NORULESSELECTED } from './ds-constants';
 import LoadingSpinner from 'components/loading-spinner';
 
 const RulesDetails = ( { data, loading, onTagClick, onTechnologyTagClick, searchVisible, gsQuery } ) => {
   let qualityStandardsTags, technologiesTags;
   if (data){
-    qualityStandardsTags = data.qualityStandards.length > 0 ? 
+    qualityStandardsTags = data.qualityStandards.length > 0 ?
       data.qualityStandards
         .map(function(listValue, index){
           return <div key={index} className={CLASSES.tag} onClick={() => {
@@ -16,7 +16,7 @@ const RulesDetails = ( { data, loading, onTagClick, onTechnologyTagClick, search
               onTagClick(listValue);
           }}>{listValue.id}</div>;
         }) : [];
-    technologiesTags = data.technologies.length > 0 ? 
+    technologiesTags = data.technologies.length > 0 ?
       data.technologies.map((t, i)=>{
         return <div key={i+'techno'} className={CLASSES.technoTag} onClick={() => {
           if(!searchVisible || gsQuery !== t.id)
@@ -26,14 +26,18 @@ const RulesDetails = ( { data, loading, onTagClick, onTechnologyTagClick, search
   }
   return (
     <div className={createClassName(COMMON_CLASSES.flexCol, CLASSES.detailsContainer)}>
-      { loading ? <LoadingSpinner/> : 
-        ( data ? 
+      { loading ? <LoadingSpinner/> :
+        ( data ?
           <div className={CLASSES.subContainer}>
             <div className={CLASSES.headerContainer}>
-              <h2 className={CLASSES.title}>{data.name}</h2>
+            <h2 className={CLASSES.title}>{(isEcho() ? (data.alternativeName ? data.alternativeName : data.name) : data.name)}</h2>
+            { isEcho() ? ( data.critical ?
+
+<div className={createClassName(CLASSES.weightContainer,COMMON_CLASSES.critical)}></div> : undefined)
+               : (
               <div className={createClassName(CLASSES.weightContainer, data.critical ? COMMON_CLASSES.critical : CLASSES.weightIcon)}>
                 <span className={CLASSES.weight}>{data.maxWeight}</span>
-              </div>
+              </div>)}
             </div>
             <div className={CLASSES.tagContainer}>
               {qualityStandardsTags}
@@ -41,10 +45,10 @@ const RulesDetails = ( { data, loading, onTagClick, onTechnologyTagClick, search
             <div className={CLASSES.tagContainer}>
               {technologiesTags}
             </div>
-            {data.description ? <div className={CLASSES.descriptionContainer}>
+            {isEcho() ? undefined : ( data.description ? <div className={CLASSES.descriptionContainer}>
               <p className={CLASSES.textArea}>Description</p>
               <p>{data.description}</p>
-            </div> : undefined}
+            </div> : undefined ) }
             {data.rationale ? <div className={CLASSES.rationaleContainer}>
               <p className={CLASSES.textArea}>Rationale</p>
               <p>{data.rationale}</p>
@@ -81,7 +85,7 @@ function parseLinks( text ){
   let str = [];
 
   for (let i = 0; i < ml; i++) {
-    const url = matches[i];    
+    const url = matches[i];
     idxStart = text.indexOf(url);
     const node = (<span key={'node-'+i}>{text.substring(idxEnd, idxStart)}</span>),
       ahrefBlock = (<a key={'anchor-'+i} href={url}>{url}</a>);
