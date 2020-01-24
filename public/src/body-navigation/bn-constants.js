a7fdb46c7362e3716dc4a970cd1990c725cea124
Guillaume Rager <g.rager@castsoftware.com>
2020-01-24 18:45:31 +0100

    added new resources and fix PCI 3.2.1

--- public/src/body-navigation/bn-constants.js
+++ public/src/body-navigation/bn-constants.js
@@ -17,7 +17,7 @@ export const SECTIONS = {
   cast: 'cast',
   cwe: 'cwe',
   nist: 'nist-sp-800-53r4',
-  pci: 'pci-dss-v3.1',
+  pci: 'pci-dss-v3.2.1',
   stig: 'stig-v4r8',
   omg: 'omg-ascqm'
 };
