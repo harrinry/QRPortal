ea674f420665e7dffe3f29f6d9d574b94c37bd10
Matteo Stura <MSU@corp.castsoftware.com>
2021-04-08 15:42:27 +0200

    TECHNOPLAT-153: fixed case sensitive image names

--- server/images/imgRouter.js
+++ server/images/imgRouter.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const { imagesRoute } = require('../routes/routes');
 const options = require('./options');
 const errHandler = require('../middleware/errorHandler');
+let fileList = [];
 
 let imageRouter = express.Router({caseSensitive: false});
 
-imageRouter.get(imagesRoute, (req, res)=>{
-  res.sendFile(req.params.imageName, options, (err)=>errHandler(err, res));
+function populateImageList(){
+  const imageList = fs.readdirSync(path.resolve(__dirname, 'img'));
+  fileList = imageList;
+}
+
+function getImageNormalizedName(name = ''){
+  if(fileList.length === 0) populateImageList();
+  return fileList.find(_ => _.toLowerCase() === name.toLowerCase());
+}
+
+imageRouter.get(imagesRoute, (req, res) => {
+  const normalizedImageName = getImageNormalizedName(req.params.imageName);
+
+  res.sendFile(normalizedImageName || req.params.imageName, options, (err)=>errHandler(err, res));
 });
 
 module.exports = imageRouter;
