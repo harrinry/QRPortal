87263985d2e015261ca72ed18173fa2a0463ae2f
Matteo Stura <MSU@corp.castsoftware.com>
2021-12-24 12:08:52 +0100

    TECHNOPLAT-209: fixed excess server queries

--- src/client/src/app/tree/crd-tree-component.js
+++ src/client/src/app/tree/crd-tree-component.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useState, useEffect, useMemo } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
 import _get from 'lodash/get';
 import _isEmpty from 'lodash/isEmpty';
 import PropTypes from 'prop-types';
@@ -14,6 +14,7 @@ const Tree = (props) => {
   const classes = useStyles();
   const { mainMenu, treeData, getTreeData, searchTerm, resetSearch, resetTreeData } = props;
   const { pathname } = useLocation();
+  const history = useHistory();
   const splittedUrls = pathname.toLowerCase().split('/');
   const mainUrl = splittedUrls[2]
     ? `${splittedUrls[1]}/${splittedUrls[2]}`
@@ -38,8 +39,27 @@ const Tree = (props) => {
     return mainMenu.items.findIndex(item => item.href === indexUrl);
   };
 
+  const reverseNodeSearch = (nodeId, index, tree) => {
+    /** @type {Array<string>} */
+    const nodeSplit = nodeId.split("/");
+    const nodeLength = nodeSplit.length;
+    const maxDepth = [2, 2, 5, 2];
+
+    if(tree[index][nodeId]) {
+      return true;
+    } else if(nodeLength <= maxDepth[index]){
+      return false;
+    } else {
+      return reverseNodeSearch(nodeSplit.slice(0, nodeLength - 1).join("/"), index, tree);
+    }
+  }
+
   const onNodeSelect = (event, nodeId) => {
-    getTreeData(nodeId, findIndex(nodeId));
+    const idx = findIndex(nodeId);
+
+    if(!reverseNodeSearch(nodeId, idx, treeDataInfo)){
+      getTreeData(nodeId, idx);
+    }
     resetSearch();
   };
 
@@ -103,26 +123,35 @@ const Tree = (props) => {
     </Tooltip>
   );
 
+  const handleNavigate = (urlFragment) => 
+    () => {
+      history.replace(urlFragment);
+    }
+
   const renderTreeItem = nodes => (
     nodes.map(node => (
-      <Link className='link-element'
-        key={node.name}
-        to={(!isCountNotValid(node)) ? `/${node.href}` : '#'}
-      >
+      // <Link className='link-element'
+      //   key={node.name}
+      //   to={(!isCountNotValid(node)) ? `/${node.href}` : '#'}
+      // >
         <TreeItem
+          key={node.name}
           disabled={isCountNotValid(node)}
           nodeId={node.href.toLowerCase()}
           label={renderLabel((node.displayName || node.name), node.description)}
           icon={node.iconUrl && renderLabelIcon(node.iconUrl)}
+          onClick={handleNavigate(!isCountNotValid(node) ? `/${node.href}` : '#')}
         >
           {
             Array.isArray(renderNeededData(node.href)[node.href.toLowerCase()])
               ? renderTreeItem(renderNeededData(node.href)[node.href.toLowerCase()])
-              : [<div />]
+              : [<div key={"void-element"}/>]
           }
         </TreeItem>
-      </Link>
+      // </Link>
     )));
+
+  const renderedTree = useMemo(() => (renderTreeItem(treeDataInfo)), [ JSON.stringify(treeDataInfo) ]);
 
   const isReloadValid = () => mainUrl && !_isEmpty(mainUrl) && !_isEmpty(mainMenu) && !searchTerm;
 
@@ -150,7 +179,7 @@ const Tree = (props) => {
         defaultExpanded={_get(defaultTreeInfo, 'defaultExpanded')}
         defaultSelected={_get(defaultTreeInfo, 'defaultSelected')}
       >
-        {renderTreeItem(treeDataInfo)}
+        {renderedTree}
       </TreeView>
     </StyledEngineProvider>
   );
